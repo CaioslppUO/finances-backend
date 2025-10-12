99b27b690ff6410b410f583070d97b826f986d8c
Caio Cezar das Neves Moreira <caio.moreira@itaipuparquetec.org.br>
2025-10-11 22:41:57 -0300

    Add expenses auxiliar models.

--- expenses/admin.py
+++ expenses/admin.py
@@ -2,13 +2,37 @@
 from django.contrib import admin
 
 # Models
-from .models import ExpenseMonth
+from .models import ExpenseMonth, ExpenseType, ExpenseBudget, ExpensePayment
 
 
 # Registers
 @admin.register(ExpenseMonth)
 class ExpenseMonthAdmin(admin.ModelAdmin):
-    list_display = ("date", "fk_user_id")
-    list_filter = ("date", "fk_user_id")
-    search_fields = ("date", "fk_user_id")
-    ordering = ("date",)
+    list_display = ("expense_month_id", "date", "fk_user_id")
+    list_filter = ("expense_month_id", "date", "fk_user_id")
+    search_fields = ("expense_month_id", "date", "fk_user_id")
+    ordering = ("expense_month_id", "date")
+
+
+@admin.register(ExpenseType)
+class ExpenseTypeAdmin(admin.ModelAdmin):
+    list_display = ("expense_type_id", "type", "fk_user_id", "is_active")
+    list_filter = ("expense_type_id", "type", "fk_user_id", "is_active")
+    search_fields = ("expense_type_id", "type", "fk_user_id", "is_active")
+    ordering = ("expense_type_id", "type", "is_active")
+
+
+@admin.register(ExpenseBudget)
+class ExpenseBudgetAdmin(admin.ModelAdmin):
+    list_display = ("expense_budget_id", "budget", "fk_user_id", "is_active")
+    list_filter = ("expense_budget_id", "budget", "fk_user_id", "is_active")
+    search_fields = ("expense_budget_id", "budget", "fk_user_id", "is_active")
+    ordering = ("expense_budget_id", "budget", "is_active")
+
+
+@admin.register(ExpensePayment)
+class ExpensePaymentAdmin(admin.ModelAdmin):
+    list_display = ("expense_payment_id", "payment", "fk_user_id", "is_active")
+    list_filter = ("expense_payment_id", "payment", "fk_user_id", "is_active")
+    search_fields = ("expense_payment_id", "payment", "fk_user_id", "is_active")
+    ordering = ("expense_payment_id", "payment", "is_active")
--- expenses/models.py
+++ expenses/models.py
@@ -9,8 +9,87 @@ class ExpenseMonth(models.Model):
     Montly based expense records.
     """
 
-    expense_record_id = models.AutoField(primary_key=True)
+    expense_month_id = models.AutoField(primary_key=True)
     fk_user_id = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="expense_months"
     )
     date = models.DateField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["fk_user_id", "date"], name="unique_user_month"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.fk_user_id.username} - {self.date.strftime('%m-%Y')}"
+
+
+class ExpenseType(models.Model):
+    """
+    Personal expense types.
+    """
+
+    expense_type_id = models.AutoField(primary_key=True)
+    fk_user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="expense_types"
+    )
+    type = models.CharField(max_length=25)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["fk_user_id", "type"], name="unique_user_expense_type"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.fk_user_id.username} - {self.type} - Active: {self.is_active}"
+
+
+class ExpenseBudget(models.Model):
+    """
+    Personal expense budgets.
+    """
+
+    expense_budget_id = models.AutoField(primary_key=True)
+    fk_user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="expense_budgets"
+    )
+    budget = models.CharField(max_length=25)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["fk_user_id", "budget"], name="unique_user_expense_budget"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.fk_user_id.username} - {self.budget} - Active: {self.is_active}"
+
+
+class ExpensePayment(models.Model):
+    """
+    Personal expense payments methods.
+    """
+
+    expense_payment_id = models.AutoField(primary_key=True)
+    fk_user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="expense_payments"
+    )
+    payment = models.CharField(max_length=25)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["fk_user_id", "payment"], name="unique_user_expense_payment"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.fk_user_id.username} - {self.payment} - Active: {self.is_active}"
