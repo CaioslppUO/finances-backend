4acd15b9c2c4429162ad438cb43363ed07ec0de3
Caio Cezar das Neves Moreira <caio.moreira@itaipuparquetec.org.br>
2026-01-14 20:52:47 -0300

    Adiciona testes para as despesas.

--- expenses/tests/fixtures.py
+++ expenses/tests/fixtures.py
@@ -5,34 +5,59 @@ from django.contrib.auth.models import User
 import pytest
 
 # Models
-from expenses.models import ExpenseType, ExpenseBudget, ExpensePayment
+from expenses.models import ExpenseType, ExpenseBudget, ExpensePayment, Expense
 
 @pytest.fixture
 def expense_type_fixture(user: User):
-    return ExpenseType.objects.bulk_create(
-        [
-            ExpenseType(fk_user_id=user, type="Expense Type 1"),
-            ExpenseType(fk_user_id=user, type="Expense Type 2"),
-            ExpenseType(fk_user_id=user, type="Expense Type 3"),
-        ]
-    )
+    return [
+        ExpenseType.objects.create(fk_user_id=user, type="Expense Type 1"),
+        ExpenseType.objects.create(fk_user_id=user, type="Expense Type 2"),
+        ExpenseType.objects.create(fk_user_id=user, type="Expense Type 3"),
+    ]
+    
 
 @pytest.fixture
 def expense_budget_fixture(user: User):
-    return ExpenseBudget.objects.bulk_create(
-        [
-            ExpenseBudget(fk_user_id=user, budget="Expense Budget 1"),
-            ExpenseBudget(fk_user_id=user, budget="Expense Budget 2"),
-            ExpenseBudget(fk_user_id=user, budget="Expense Budget 3"),
-        ]
-    )
+    return [
+        ExpenseBudget.objects.create(fk_user_id=user, budget="Expense Budget 1"),
+        ExpenseBudget.objects.create(fk_user_id=user, budget="Expense Budget 2"),
+        ExpenseBudget.objects.create(fk_user_id=user, budget="Expense Budget 3"),
+    ]
 
 @pytest.fixture
 def expense_payment_fixture(user: User):
-    return ExpensePayment.objects.bulk_create(
-        [
-            ExpensePayment(fk_user_id=user, payment="Expense Payment 1"),
-            ExpensePayment(fk_user_id=user, payment="Expense Payment 2"),
-            ExpensePayment(fk_user_id=user, payment="Expense Payment 3"),
-        ]
-    )
+    return [
+        ExpensePayment.objects.create(fk_user_id=user, payment="Expense Payment 1"),
+        ExpensePayment.objects.create(fk_user_id=user, payment="Expense Payment 2"),
+        ExpensePayment.objects.create(fk_user_id=user, payment="Expense Payment 3"),
+    ]
+
+@pytest.fixture
+def expense_fixture(user: User, expense_type_fixture, expense_budget_fixture, expense_payment_fixture):
+    return [
+        Expense.objects.create(fk_user_id=user,
+            description="Expense 1",
+            value=100.52,
+            date="2026-01-01",
+            fk_type_id=expense_type_fixture[0],
+            fk_budget_id=expense_budget_fixture[0],
+            fk_payment_id=expense_payment_fixture[0],
+        ),
+        Expense.objects.create(fk_user_id=user,
+            description="Expense 2",
+            value=39.12,
+            date="2026-01-15",
+            fk_type_id=expense_type_fixture[1],
+            fk_budget_id=expense_budget_fixture[1],
+            fk_payment_id=expense_payment_fixture[1],
+        ),
+        Expense.objects.create(fk_user_id=user,
+            description="Expense 3",
+            value=20,
+            date="2026-01-09",
+            fk_type_id=expense_type_fixture[2],
+            fk_budget_id=expense_budget_fixture[2],
+            fk_payment_id=expense_payment_fixture[2],
+        ),
+    ]
+    
---
+++ expenses/tests/test_expense.py
@@ -0,0 +1,110 @@
+# Django
+from django.urls import reverse
+from django.db import IntegrityError
+from rest_framework.test import APIClient
+
+# Pytest
+import pytest
+
+# Models
+from expenses.models import ExpensePayment
+
+@pytest.mark.django_db
+def test_create_expense(auth_client: APIClient, expense_payment_fixture, expense_budget_fixture, expense_type_fixture):
+    # Rota
+    url = reverse("expenses-list")
+
+    # Cria uma despesa para testar depois
+    payload = {
+        "description": "New Expense Description",
+        "value": "125.56",
+        "date": "2026-01-01",
+        "fk_type_id": 1,
+        "fk_budget_id": 1,
+        "fk_payment_id": 1
+    }
+    response = auth_client.post(url, payload, format="json")
+
+    # Testes
+    assert response.status_code == 201
+    assert response.data["expense_id"] == 1
+    assert response.data["description"] == "New Expense Description"
+    assert response.data["value"] == "125.56"
+    assert response.data["date"] == "2026-01-01"
+    assert response.data["fk_type_id"] == 1
+    assert response.data["fk_budget_id"] == 1
+    assert response.data["fk_payment_id"] == 1
+
+@pytest.mark.django_db
+def test_get_expense(auth_client: APIClient, expense_fixture) -> None:
+    # Rota
+    url = reverse("expenses-list")
+    response = auth_client.get(url)
+
+    # Testes
+    assert response.status_code == 200
+    assert len(response.data) == 3
+
+    # Verifica os dados preenchidos pelas fixtures
+    assert response.data[0]["description"] == "Expense 1"
+    assert response.data[0]["value"] == "100.52"
+    assert response.data[0]["date"] == "2026-01-01"
+    assert response.data[0]["fk_type_id"] == 1
+    assert response.data[0]["fk_budget_id"] == 1
+    assert response.data[0]["fk_payment_id"] == 1
+
+    assert response.data[1]["description"] == "Expense 2"
+    assert response.data[1]["value"] == "39.12"
+    assert response.data[1]["date"] == "2026-01-15"
+    assert response.data[1]["fk_type_id"] == 2
+    assert response.data[1]["fk_budget_id"] == 2
+    assert response.data[1]["fk_payment_id"] == 2
+
+    assert response.data[2]["description"] == "Expense 3"
+    assert response.data[2]["value"] == "20.00"
+    assert response.data[2]["date"] == "2026-01-09"
+    assert response.data[2]["fk_type_id"] == 3
+    assert response.data[2]["fk_budget_id"] == 3
+    assert response.data[2]["fk_payment_id"] == 3
+
+@pytest.mark.django_db
+def test_update_expense(auth_client: APIClient, expense_fixture) -> None:
+    # Rota
+    url = reverse("expenses-list")
+
+    # Antes de Editar
+    before_edit = auth_client.get(url)
+    assert before_edit.data[0]["description"] == "Expense 1"
+
+    # Edição
+    payload = {
+        "description": "Expense Description 1 Updated"
+    }
+    response_edit = auth_client.patch(url + str(before_edit.data[0]["expense_id"])  + "/", payload, format="json")
+    assert response_edit.status_code == 200
+    assert response_edit.data["description"] == "Expense Description 1 Updated"
+    
+    # Depois de editar
+    after_edit = auth_client.get(url)
+    assert after_edit.data[0]["description"] == "Expense Description 1 Updated"
+    assert after_edit.data[1]["description"] == "Expense 2"
+    assert after_edit.data[2]["description"] == "Expense 3"
+
+@pytest.mark.django_db
+def test_delete_expense(auth_client: APIClient, expense_fixture) -> None:
+    # Rota
+    url = reverse("expenses-list")
+
+    # Antes de Editar
+    before_edit = auth_client.get(url)
+    assert before_edit.data[0]["description"] == "Expense 1"
+
+    # Deleção
+    response_edit = auth_client.delete(url + str(before_edit.data[0]["expense_id"])  + "/")
+    assert response_edit.status_code == 204
+    
+    # Depois de deletar
+    after_delete = auth_client.get(url)
+    assert after_delete.data[0]["description"] == "Expense 2"
+    assert after_delete.data[1]["description"] == "Expense 3"
+    assert len(after_delete.data) == 2
